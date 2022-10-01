b4bf2ca737fec634d89d955e6e189087aa2f378f
Garret <garretthecastro@gmail.com>
2022-10-01 15:14:48 -0700

    Change Block Comments to Single Line Comments
    Single-line comments are easier to read; each individual line of code can be more easily analyzed in a vacuum or without an editor changing the font/color.
    
    Block comments are more useful for quickly disabling large swaths of code for debugging instead of documentation.

--- src/generate.rb
+++ src/generate.rb
@@ -1,29 +1,28 @@
 #!/usr/bin/ruby
 
-=begin
+#
+#     Copyright 2022 Jacob Bates
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+#
 
-    Copyright 2022 Jacob Bates
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+# TODO: - Sorta-Tables (tab a line, do part of a line in left-align and part in right)
 
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-
-=end
-
-# TODO - Sorta-Tables (tab a line, do part of a line in left-align and part in right)
-
-# TODO - PDF Output
+# TODO: - PDF Output
 # TODO - Write Stylesheets
 
-# TODO - Testing/Debugging (escape sequences in source, things missing from stylesheet, invalid input)
+# TODO: - Testing/Debugging (escape sequences in source, things missing from stylesheet, invalid input)
 
 # Other Classes
 require_relative "stylesheet.rb"
